bf5dfd8f64b8367a2af4e22ed43828e8da1f34b9
Grant <gsteere4717@xmail.dixietech.edu>
2024-06-27 10:52:06 -0600

    Add calculator

--- react-calculator/src/App.jsx
+++ react-calculator/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
-
+import Calculator from './Calculator';
 export default function App() {
 
   return (
 
     <>
+    <header>
+      <h1>Calculator</h1>
+    </header>
+    <Calculator/>
+    <footer>
+      <p>Created by Grant Steere</p>
+    </footer>
     </>
   );
 }
